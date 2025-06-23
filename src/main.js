39145e5bef216c520b946cb06d4ccf55abbb4b62
Kilian Rempen <kilianrempen@gmail.com>
2025-06-22 23:42:29 -0600

    milestone 3 work

--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
 import './index.css'
 import 'flowbite'
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var currentYear = new Date().getFullYear();
+    document.getElementById('currentYear').textContent = currentYear;
+});
