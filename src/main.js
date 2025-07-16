e17426981b1ee869354a4516e85fec8a4ce1244c
Kilian Rempen <kilianrempen@gmail.com>
2025-07-15 22:44:30 -0600

    updates

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import './index.css'
 import 'flowbite'
+import {contactForm} from "../contact-form.js";
 
+contactForm()
 
 document.addEventListener('DOMContentLoaded', function() {
     var currentYear = new Date().getFullYear();
