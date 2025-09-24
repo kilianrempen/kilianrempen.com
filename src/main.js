721b7bdfaf50074a1a9465de077e95c9b8ca0b24
Kilian Rempen <kilianrempen@gmail.com>
2025-09-24 16:37:08 -0600

    draft 1 of moving mobile menu icon

--- src/main.js
+++ src/main.js
@@ -163,3 +163,17 @@ function randomCharDifferent(correct) {
 }
 function randomInt(min, max) { return Math.floor(Math.random() * (max - min + 1)) + min; }
 function wait(ms) { return new Promise(r => setTimeout(r, ms)); }
+
+//MOBILE HEADER BUTTON MOVEMENT
+const navbar = document.querySelector('nav');
+window.addEventListener('scroll', () => {
+    const flexDiv = document.getElementById('mobile-menu-button');
+    const navbarTop = navbar.getBoundingClientRect().top;
+    if (Math.abs(navbarTop) < 2) {
+        flexDiv.classList.remove('justify-center');
+        flexDiv.classList.add('justify-end');
+    } else {
+        flexDiv.classList.remove('justify-end');
+        flexDiv.classList.add('justify-center');
+    }
+});
